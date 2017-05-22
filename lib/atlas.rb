b9ca51c2d62e7eb86b830d40a703bb5a10d01d7c
Gabriel Teles <gab.teles@hotmail.com>
2017-05-22 13:43:42 -0300

    [fix] requiring dependencies

--- lib/atlas.rb
+++ lib/atlas.rb
@@ -1,8 +1,11 @@
 require 'active_support/dependencies'
 require 'active_support/core_ext/object/try'
 require 'active_support/time_with_zone'
+require 'aws-sdk'
 require 'dry-validation'
+require 'hanami-controller'
 require 'ice_nine'
+require 'mongoid'
 require 'rack'
 
 module Atlas
