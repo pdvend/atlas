bd09602299fbcea4892b2486f98fbe3d7d595799
Gabriel Teles <gab.teles@hotmail.com>
2017-06-26 17:45:18 -0300

    [feat] adding validation hook

---
+++ lib/atlas/hook/validation_hook.rb
@@ -0,0 +1,42 @@
+module Atlas
+  module Hook
+    class ValidationHook
+      include Atlas::Util::I18nScope
+
+      VALID_PARAMS = Atlas::Service::ServiceResponse.new(data: {}, code: Enum::ErrorCodes::NONE).freeze
+      EVALUATION_METHODS = { raw: :raw_evaluate, schema: :schema_evaluate }.freeze
+
+      def execute(context, params, evaluation_type = :raw, &block)
+        evaluation_method = EVALUATION_METHODS[evaluation_type]
+        return VALID_PARAMS unless evaluation_method
+        method(evaluation_method).call(context, params, &block)
+      end
+
+      private
+
+      def raw_evaluate(context, params)
+        yield(context, params) ? VALID_PARAMS : invalid_params({})
+      end
+
+      def schema_evaluate(context, params, &block)
+        result = schema_for(block).call(@parameters)
+        result.success? ? VALID_PARAMS : invalid_params(result.errors)
+      end
+
+      def schema_for(block)
+        Dry::Validation.Schema do
+          configure { config.messages = :i18n }
+          instance_eval(&block)
+        end
+      end
+
+      def invalid_params(errors)
+        Atlas::Service::ServiceResponse.new(
+          message: I18n.t(:invalid_params, scope: i18n_scope),
+          data: errors,
+          code: Enum::ErrorCodes::VALIDATION,
+        )
+      end
+    end
+  end
+end
