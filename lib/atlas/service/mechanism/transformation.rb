72d3c08fb22c8e55ba60d85b73c95ee3c0b3eca8
Romario Clacino <romarioclacino@gmail.com>
2017-08-31 19:56:37 -0300

    creation of lib/atlas/service/mechanism/transformation.rb

---
+++ lib/atlas/service/mechanism/transformation.rb
@@ -0,0 +1,31 @@
+module Atlas
+  module Service
+    module Mechanism
+      class Transformation
+        extend Atlas::Util::I18nScope
+
+        OPERATORS = %i[sum].freeze
+        OPERANTION_PARTS_SEPARATOR = /(#{OPERATORS.join('|')}):(.*)/
+
+        def self.transformation_params(params, entity)
+          raw_statments_parts = params.match(OPERANTION_PARTS_SEPARATOR).to_a
+          parts = { operator: raw_statments_parts[1], field: raw_statments_parts[2] }
+          response(entity, parts)
+        end
+
+        def self.response(entity, parts)
+          return response_error(:invalid_operator) unless OPERATORS.include?(parts[:operator].try(:to_sym))
+          return response_error(:invalid_field) unless entity.instance_parameters.include?(parts[:field].try(:to_sym))
+          ServiceResponse.new(data: parts, code: Enum::ErrorCodes::NONE)
+        end
+        private_class_method :response
+
+        def self.response_error(message_key)
+          message = I18n.t(message_key, scope: i18n_scope)
+          return ServiceResponse.new(message: message, data: {}, code: Enum::ErrorCodes::PARAMETER_ERROR)
+        end
+        private_class_method :response_error
+      end
+    end
+  end
+end
---
+++ spec/lib/service/mechanism/transformation_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe Atlas::Service::Mechanism::Transformation do
+  describe '.sorting_params' do
+    subject { described_class.transformation_params(params, entity) }
+    let(:entity) { Atlas::Spec::Mock::Entity[:value] }
+
+    context 'with valid field but operator does not exist' do
+      let(:params) { 'invalid_operator:value' }
+      it { is_expected.not_to be_success }
+      it { expect(subject.code).to eq(Atlas::Enum::ErrorCodes::PARAMETER_ERROR) }
+      it { expect(subject.message).to eq('Invalid operator') }
+    end
+
+    context 'with valid operator but field does not exist' do
+      let(:params) { 'sum:invalid_field' }
+      it { is_expected.not_to be_success }
+      it { expect(subject.code).to eq(Atlas::Enum::ErrorCodes::PARAMETER_ERROR) }
+      it { expect(subject.message).to eq('Invalid field') }
+    end
+
+    context 'when valid' do
+      let(:params) { 'sum:value' }
+      it { is_expected.to be_success }
+      it { expect(subject.code).to eq(Atlas::Enum::ErrorCodes::NONE) }
+      it { expect(subject.data).to eq(operator: 'sum', field: 'value') }
+    end
+  end
+end
