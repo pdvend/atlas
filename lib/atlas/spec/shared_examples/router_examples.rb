b2836071acc2f1f51aa7678d2ff7852ffe953e8b
Maria Luciene <marialucienefelix@gmail.com>
2017-09-01 17:35:35 -0300

    Adding shared examples to router class
    - routable endpoints

---
+++ lib/atlas/spec/shared_examples/router_examples.rb
@@ -0,0 +1,21 @@
+require 'rspec/core/shared_context'
+
+module Atlas
+  module Spec
+    module SharedExamples
+      module RouterExamples
+        extend RSpec::Core::SharedContext
+
+        shared_examples_for('a routable endpoint') do |method_type, path, params = {}|
+          subject { described_class.recognize(env) }
+          let(:env) { Rack::MockRequest.env_for(path, method: method_type) }
+
+          it { expect(subject).to be_routable }
+          it { expect(subject.path).to eq(path) }
+          it { expect(subject.verb).to eq(method_type) }
+          it { expect(subject.params).to eq(params) }
+        end
+      end
+    end
+  end
+end
