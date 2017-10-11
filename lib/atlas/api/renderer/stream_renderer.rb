02298b79df1f9096184d1914eaaf3e7fe65591a9
Gabriel Teles <gab.teles@hotmail.com>
2017-10-11 07:54:27 -0300

    [fix] keep headers untouched when streaming enumerator

--- lib/atlas/api/renderer/stream_renderer.rb
+++ lib/atlas/api/renderer/stream_renderer.rb
@@ -3,11 +3,11 @@ module Atlas
     module Renderer
       class StreamRenderer < JsonRenderer
         def headers
-          super.merge('Content-Type' => 'application/json')
+          base = super
+          body_data.is_a?(Enumerator) ? base : base.merge('Content-Type' => 'application/json')
         end
 
         def body
-          return error_from(service_response).to_json unless service_response.success?
           data = body_data
           return data if data.is_a?(Enumerator)
           Enumerator.new { |yielder| yielder << super }
