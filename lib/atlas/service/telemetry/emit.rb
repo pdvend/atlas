deb5c0079ddce8406c63e43c3949e2b272827158
Gabriel Teles <gab.teles@hotmail.com>
2017-10-19 15:45:47 -0200

    [refact] fixing reek warnings

--- lib/atlas/service/telemetry/emit.rb
+++ lib/atlas/service/telemetry/emit.rb
@@ -14,16 +14,18 @@ module Atlas
           type = event.try(:[], :type)
           data = event.try(:[], :data)
 
-          unless valid_params(context, type, data)
-            message = I18n.t(:invalid_parameters, scope: i18n_scope)
-            return ServiceResponse.new(message: message, data: {}, code: Enum::ErrorCodes::PARAMETER_ERROR)
-          end
+          return invalid_parameters unless valid_params(context, type, data)
 
           @adapter.log(type, data.merge(context.to_event))
           ServiceResponse.new(data: {}, code: Enum::ErrorCodes::NONE)
         end
 
         private
+
+        def invalid_parameters
+          message = I18n.t(:invalid_parameters, scope: i18n_scope)
+          ServiceResponse.new(message: message, data: {}, code: Enum::ErrorCodes::PARAMETER_ERROR)
+        end
 
         def valid_params(context, type, data)
           context.is_a?(Service::RequestContext) && type.is_a?(Symbol) && data.is_a?(Hash)
