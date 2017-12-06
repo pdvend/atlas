5abb9238de668c999e9a63480b501873ef2ee570
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2017-12-06 17:58:07 -0200

    Fix

--- lib/atlas/job/noop.rb
+++ lib/atlas/job/noop.rb
@@ -5,27 +5,7 @@ module Atlas
     class Noop
       include Concurrent::Async if defined?(Concurrent)
 
-      JobWrapper = Struct.new(:notifier, :job_class, :payload) do
-        extend Forwardable
-
-        def perform
-        end
-
-        def_delegator :job_class, :retries, :max_attempts
-        def_delegator :job_class, :topic, :queue_name
-
-        def reschedule_at(current_time, _attempts)
-          current_time + job_class.timeout_delay
-        end
-
-        def error(_job, error)
-        end
-
-        def failure(_job)
-        end
-      end
-
-      def enqueue(job, payload: {}, delay: 0)
+      def enqueue(_job, payload: {}, delay: 0)
       end
 
       def process
