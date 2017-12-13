121730f7e28d80bba84bf12543c7aae21f6be235
Thiago Ribeiro <thiagitosouza@gmail.com>
2017-12-13 09:37:43 -0200

    fix format number

--- lib/atlas/enum/formats.rb
+++ lib/atlas/enum/formats.rb
@@ -5,7 +5,7 @@ module Atlas
     module Formats
       UUID4 = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}/ix
       VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-      VALID_PHONE_NUMBER = /^\(\s*(?<ddd>\d\d)\s*\)\s*(?<part1>\d{4,5})\s*(?:\s|\.|-)?\s*(?<part2>\d{4})$/
+      VALID_PHONE_NUMBER = /\d{8,11}/i
     end
   end
 end
