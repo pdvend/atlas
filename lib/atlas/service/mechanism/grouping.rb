ac22162cf180034d40b99e483d7be9564047efa6
Gabriel Teles <gab.teles@hotmail.com>
2018-02-27 10:58:36 -0300

    fix(grouping) condition

--- lib/atlas/service/mechanism/grouping.rb
+++ lib/atlas/service/mechanism/grouping.rb
@@ -11,7 +11,7 @@ module Atlas
         def self.group_params(params, entity)
           group_field, *raw_transformations = params.try(:split, DIVIDER_FLAG)
 
-          return false if !entity || !group_field || !entity.can_group_by?(group_field)
+          return false if entity.present? && group_field.present? && !entity.can_group_by?(group_field)
 
           transformations = raw_transformations.lazy
               .map { |field| generate_grouping_statement(field) }
