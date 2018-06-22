b73ac922c07e8794a487bd7c662008fdb2e4f0ed
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2018-06-22 09:34:03 -0300

    File Storage complete

---
+++ lib/atlas/repository/file_storage_repository.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+module Atlas
+  module Repository
+    class FileStorageRepository
+      EMPTY_STRING = ''
+      private_constant :EMPTY_STRING
+
+      DEFAULT_ERR_CODE = Enum::ErrorCodes::REPOSITORY_INTERNAL
+
+      def initialize(notifier:, base_path:)
+        @notifier = notifier
+        @base_path = base_path
+      end
+
+      def put(uuid, content)
+        return failure unless valid_object_identifier?(uuid) && content.is_a?(String)
+
+        wrap do
+          save_file(content, uuid)
+          Atlas::Repository::RepositoryResponse.new(data: nil, err_code: Enum::ErrorCodes::NONE)
+        end
+      end
+
+      def content(uuid)
+        return failure unless valid_object_identifier?(uuid)
+        wrap { file_content(uuid) }
+      end
+
+      def handle(uuid)
+        return failure unless valid_object_identifier?(uuid)
+        wrap { file_handle(uuid) }
+      end
+
+      protected
+
+      # :nocov:
+      def bucket_name
+        raise 'Implement the method #bucket_name in order to use BaseMongoidRepository.'
+      end
+      # :nocov:
+
+      def base_folder
+        EMPTY_STRING
+      end
+
+      private
+
+      attr_accessor :notifier, :base_path
+
+      def wrap
+        yield
+      rescue Aws::S3::Errors::NoSuchKey => message
+        failure(message: message, code: Enum::ErrorCodes::DOCUMENT_NOT_FOUND)
+      rescue Aws::S3::Errors::ServiceError => message
+        failure(message: message, code: DEFAULT_ERR_CODE)
+      end
+
+      def valid_object_identifier?(uuid)
+        uuid.is_a?(String) && uuid.present?
+      end
+
+      def file_content(uuid)
+        data = File.binread("#{base_path}/#{uuid}")
+        Atlas::Repository::RepositoryResponse.new(data: data, err_code: Enum::ErrorCodes::NONE)
+      end
+
+      def file_handle(uuid)
+        data = File.open("#{base_path}/#{uuid}", File::RDONLY | File::BINARY)
+        Atlas::Repository::RepositoryResponse.new(data: data, err_code: Enum::ErrorCodes::NONE)
+      end
+
+      def save_file(content, dest)
+        File.binwrite("#{base_path}/#{dest}", content)
+      end
+
+      def failure(message: nil, code: DEFAULT_ERR_CODE)
+        notifier.send_error(message) if code == DEFAULT_ERR_CODE && message.present?
+        Atlas::Repository::RepositoryResponse.new(data: nil, err_code: code)
+      end
+    end
+  end
+end
