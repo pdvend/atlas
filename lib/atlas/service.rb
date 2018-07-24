e04c0a38fbee13a6691576bb3804119e98e50544
Thiago Ribeiro <thiagitosouza@gmail.com>
2018-07-24 14:52:53 -0300

    reorder service requires

--- lib/atlas/service.rb
+++ lib/atlas/service.rb
@@ -2,11 +2,11 @@
 
 module Service
   require_relative 'service/base_service'
+  require_relative 'service/util'
   require_relative 'service/mechanism'
   require_relative 'service/notifier'
   require_relative 'service/request_context'
   require_relative 'service/service_response'
   require_relative 'service/system_context'
   require_relative 'service/telemetry'
-  require_relative 'service/util'
 end
