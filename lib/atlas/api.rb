03c9fe4dc15d9e6379fd4ea1385eda2b900bbb49
Thiago Ribeiro <thiagitosouza@gmail.com>
2018-07-24 14:58:56 -0300

    load renderer before base controller

--- lib/atlas/api.rb
+++ lib/atlas/api.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module API
+  require_relative 'api/renderer'
   require_relative 'api/base_controller'
   require_relative 'api/context'
   require_relative 'api/documentation'
   require_relative 'api/middleware'
-  require_relative 'api/renderer'
   require_relative 'api/serializer'
 end
