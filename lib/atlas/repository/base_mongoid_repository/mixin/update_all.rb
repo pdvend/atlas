a53c9c12d20d99ad114463d5cf72c8431a9e5f01
Thiago Ribeiro <thiagitosouza@gmail.com>
2018-09-04 10:59:48 -0300

    def update_all

--- lib/atlas/repository/base_mongoid_repository/mixin/update_all.rb
+++ lib/atlas/repository/base_mongoid_repository/mixin/update_all.rb
@@ -5,7 +5,7 @@ module Atlas
     class BaseMongoidRepository
       module Mixin
         module UpdateAll
-          def update(params)
+          def update_all(params)
             wrap do
               model.where(params[:query]).update_all(params[:update_params])
               Atlas::Repository::RepositoryResponse.new(data: nil, err_code: Enum::ErrorCodes::NONE)
