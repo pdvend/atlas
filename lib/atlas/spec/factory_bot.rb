5e93e31ffb7e30eab82942b7543a96d097c3af29
Thiago Ribeiro <thiagitosouza@gmail.com>
2018-10-15 11:18:45 -0300

    fix factory_bot

--- lib/atlas/spec/factory_bot.rb
+++ lib/atlas/spec/factory_bot.rb
@@ -10,49 +10,49 @@ FactoryBot.define do
     data { Object.new }
 
     trait :success do
-      err_code 1000
+      err_code { 1000 }
     end
 
     trait :failure do
-      err_code -1001
+      err_code { -1001 }
     end
   end
 
   factory :request_context, class: Atlas::Service::RequestContext do
     time { Time.now.utc }
-    component 'Shared Example Component'
+    component { 'Shared Example Component' }
     caller { 'Shared Examples' }
     transaction_id { SecureRandom.uuid }
     account_id { SecureRandom.uuid }
-    authentication_type :system
-    user nil
+    authentication_type { :system }
+    user { nil }
   end
 
   factory :service_response, class: Atlas::Service::ServiceResponse do
     trait :success do
       data { Object.new }
-      code Atlas::Enum::ErrorCodes::NONE
-      message nil
+      code { Atlas::Enum::ErrorCodes::NONE }
+      message { nil }
     end
 
     trait :failure do
-      data({})
-      code Atlas::Enum::ErrorCodes::INTERNAL
-      message 'Fake Error'
+      data { {} }
+      code { Atlas::Enum::ErrorCodes::INTERNAL }
+      message { 'Fake Error' }
     end
 
     trait :unauthorized do
-      data({})
-      code Atlas::Enum::ErrorCodes::PERMISSION_ERROR
-      message 'Unauthorized'
+      data { {} }
+      code { Atlas::Enum::ErrorCodes::PERMISSION_ERROR }
+      message { 'Unauthorized' }
     end
   end
 
   factory :job_message, class: Atlas::Job::JobMessage do
-    topic 'foo'
-    payload(foo: :bar)
-    retries 0
-    timestamp 0
-    vendor_message(bar: :baz)
+    topic { 'foo' }
+    payload { { foo: :bar } }
+    retries { 0 }
+    timestamp { 0 }
+    vendor_message { { bar: :baz } }
   end
 end
