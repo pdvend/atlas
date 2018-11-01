ce9fdd77293d8be91e78d5da6c55f4e9d929a5f7
Thiago Ribeiro <thiagitosouza@gmail.com>
2018-11-01 11:22:34 -0300

    add sidekiq_processor

---
+++ lib/atlas/job/sidekiq_processor.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Atlas
+  module Job
+    class SidekiqProcessor
+      include Atlas::Service::Util::ResponseHelpers
+      include ::Sidekiq::Worker
+
+      JobKeeper = Class.new(StandardError)
+
+      def perform(job, payload: {}, delay: 0, priority: 5)
+        job_instance = job.new
+        results = [
+          Atlas::Enum::JobsResponseCodes::PROCESS_MESSAGE,
+          Atlas::Enum::JobsResponseCodes::FAILED_NO_RETRY
+        ]
+
+        result = job_instance.perform(payload)
+        # When job succeeds or fails and don't need retry, our job is done
+        return if results.include?(result)
+        # Else, we should force the exception to not take the job from the queue
+        notifier = Atlas::Service::Notifier::Slack.new(ENV['SLACK_WEBHOOK_URL'])
+        notifier.send_error(error, Atlas::Service::SystemContext, [], "`#{job_instance.class.name}: #{payload.to_json}`")
+        raise JobKeeper
+      end
+    end
+  end
+end
